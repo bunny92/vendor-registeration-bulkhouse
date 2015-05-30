7c441d9365547259e5b82f64d767a986a7623ccc
dev-bulkhouse <developer@bulkhouse.com>
2015-05-30 19:03:54 +0530

    login-form update (kishore)

--- application/views/forms/login_form.php
+++ application/views/forms/login_form.php
@@ -1,33 +1,22 @@
 <form>
-                            <div class="row" style="padding-top: 20px">
+                            <div class="row" style="padding-top: 5px">
                                 <div class="row">
-                                    <div class="small-11 columns">
-                                        <div class="row">
-                                            <div class="small-3 columns">
-                                                <label for="right-label" class="right inline">Email</label>
-                                            </div>
-                                            <div class="small-9 columns">
-                                                <input type="text" id="right-label" placeholder="Inline Text Input">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="small-11 columns">
-                                        <div class="row">
-                                            <div class="small-3 columns">
-                                                <label for="right-label" class="right inline">Mobile</label>
-                                            </div>
-                                            <div class="small-9 columns">
-                                                <input type="text" id="right-label" placeholder="Inline Text Input">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="large-12 columns zeropadding">
+                                    <div class="small-4 columns">
 
-                                        <input class="button expand" type="button" value="Start Selling" style="margin: 0px"/>
+                                                <input type="text" id="right-label" placeholder="Inline Text Input">
+
+                                    </div>
+
+                                    <div class="small-4 columns">
+
+                                                <input type="text" id="right-label" placeholder="Inline Text Input">
+
+                                    </div>
+
+
+                                    <div class="large-4 columns zeropadding">
+
+                                        <input class="button expand tiny" type="button" value="Start Selling" style="margin: 0px"/>
 
                                     </div>
                                 </div>
