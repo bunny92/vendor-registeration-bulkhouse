7b33095a0a3cd9211fce8db874d0547cf4f2759b
dev-bulkhouse <developer@bulkhouse.com>
2015-05-30 23:08:25 +0530

    change

--- application/views/forms/login_form.php
+++ application/views/forms/login_form.php
@@ -1,24 +1,24 @@
 <form>
-    <div class="row" style="padding-top: 5px">
-        <div class="row">
+    <div class="row">
+
             <div class="small-4 columns">
 
-                <input type="text" id="right-label" placeholder="Inline Text Input">
+                <input type="text"  placeholder="Inline Text Input">
 
             </div>
 
             <div class="small-4 columns">
 
-                <input type="text" id="right-label" placeholder="Inline Text Input">
+                <input type="text"placeholder="Inline Text Input">
 
             </div>
 
 
             <div class="large-4 columns zeropadding">
 
-                <input class="button expand tiny" type="button" value="Start Selling" style="margin: 0px"/>
+                <input class="button small" type="button" value="Start Selling" style="margin: 0px"/>
 
             </div>
         </div>
-    </div>
+
 </form>
