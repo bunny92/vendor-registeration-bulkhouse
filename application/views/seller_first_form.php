32b23e4ca7a6c46cfaa6e04fbae28b61edae27c5
dev-bulkhouse <developer@bulkhouse.com>
2015-05-30 18:28:13 +0530

    First comment (kishore)

---
+++ application/views/seller_first_form.php
@@ -0,0 +1,35 @@
+<form>
+                            <div class="row" style="padding-top: 20px">
+                                <div class="row">
+                                    <div class="small-11 columns">
+                                        <div class="row">
+                                            <div class="small-3 columns">
+                                                <label for="right-label" class="right inline">Email</label>
+                                            </div>
+                                            <div class="small-9 columns">
+                                                <input type="text" id="right-label" placeholder="Inline Text Input">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="small-11 columns">
+                                        <div class="row">
+                                            <div class="small-3 columns">
+                                                <label for="right-label" class="right inline">Mobile</label>
+                                            </div>
+                                            <div class="small-9 columns">
+                                                <input type="text" id="right-label" placeholder="Inline Text Input">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="large-12 columns zeropadding">
+
+                                        <input class="button expand" type="button" value="Start Selling" style="margin: 0px"/>
+
+                                    </div>
+                                </div>
+                            </div>
+</form>
---
+++ application/views/template/header.php
@@ -0,0 +1,5 @@
+<div class="row-fluid">
+            <div data-magellan-expedition="fixed" class="large-12 columns zeropadding">
+
+            </div>
+        </div>
